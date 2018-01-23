840fca3c0abab40c190375781591188e75ed9a4e
rosie w <rosie.wilt@gmail.com>
2018-01-23 03:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ function currentLine(line) {
   for (var i = 0; i < line.length; i++) {
     linearray.push(i)
     }
-    console.log(`The line is currently ${linearray[i]}: ${line[i]}`)
+    return `The line is currently ${linearray[i]}: ${line[i]}`
   }
