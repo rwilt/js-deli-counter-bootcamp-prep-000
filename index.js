408fbe0251c3af015e14ce697c40c2cb1e441741
rosie w <rosie.wilt@gmail.com>
2018-01-23 03:14:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ function currentLine(line) {
   if (line.length == 0) {
   console.log(`The line is currently: empty`)};
   for (var i = 0; i < line.length; i++) {
-  linearray.push(`The line is currently: ${line[i]}`)
+  return `The line is currently` linearray.push(`${line[i]}`)
   }
-  return linearray
 }
