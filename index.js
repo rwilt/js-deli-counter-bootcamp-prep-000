7d65e2d2d990317fdea692c64988431ffaf609e4
rosie w <rosie.wilt@gmail.com>
2018-01-23 03:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ function currentLine(line) {
   for (var i = 0; i < line.length; i++) {
   linearray.push(`${line[i]}`)
   }
-  return `The line is currently ${linearray}`
+  return `The line is currently ${linearray[i]}`
 }
